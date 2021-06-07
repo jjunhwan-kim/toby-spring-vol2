58631ab151211b452959a7684a917183538acb20
nefertirii <nefertiri972@gmail.com>
2021-06-08 00:50:56 +0900

    Add context hierachy learning test
    컨텍스트 계층구조 테스트
    child 컨텍스트에서 printer 빈을 찾지 못하면 parent 컨텍스트에서 찾음

--- src/test/java/tobyspring/springbook/learningtest/spring/ioc/ApplicationContextTest.java
+++ src/test/java/tobyspring/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -2,17 +2,22 @@ package tobyspring.springbook.learningtest.spring.ioc;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
 import tobyspring.springbook.learningtest.spring.ioc.bean.Hello;
+import tobyspring.springbook.learningtest.spring.ioc.bean.Printer;
 import tobyspring.springbook.learningtest.spring.ioc.bean.StringPrinter;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationContextTest {
 
@@ -76,5 +81,29 @@ class ApplicationContextTest {
         hello.print();
 
         assertThat(ac.getBean("printer").toString()).isEqualTo("Hello Spring");
+    }
+
+    @Test
+    void createContextWithoutParent() {
+        assertThrows(BeanCreationException.class, () -> new GenericXmlApplicationContext("/childContext.xml"));
+    }
+
+    @Test
+    void contextHierachy() {
+        ApplicationContext parent = new GenericXmlApplicationContext("/parentContext.xml");
+
+        GenericApplicationContext child = new GenericApplicationContext(parent);
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+        reader.loadBeanDefinitions("/childContext.xml");
+        child.refresh();
+
+        Printer printer = child.getBean("printer", Printer.class);
+        assertThat(printer).isNotNull();
+
+        Hello hello = child.getBean("hello", Hello.class);
+        assertThat(hello).isNotNull();
+
+        hello.print();
+        assertThat(printer.toString()).isEqualTo("Hello Child");
     }
 }
