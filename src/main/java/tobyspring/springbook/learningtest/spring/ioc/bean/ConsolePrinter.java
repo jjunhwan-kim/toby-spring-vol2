55bf497490e4178dcf9159cbdd3938bbe4cb41c2
nefertirii <nefertiri972@gmail.com>
2021-06-07 23:45:45 +0900

    Add pojo class for learning test
    IoC 컨테이너 학습테스트를 위한 POJO 클래스 추가

---
+++ src/main/java/tobyspring/springbook/learningtest/spring/ioc/bean/ConsolePrinter.java
@@ -0,0 +1,8 @@
+package tobyspring.springbook.learningtest.spring.ioc.bean;
+
+public class ConsolePrinter implements Printer {
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/tobyspring/springbook/learningtest/spring/ioc/bean/Printer.java
@@ -0,0 +1,5 @@
+package tobyspring.springbook.learningtest.spring.ioc.bean;
+
+public interface Printer {
+    void print(String message);
+}
